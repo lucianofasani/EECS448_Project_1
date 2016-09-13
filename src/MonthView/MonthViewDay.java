6c7d2fa593df9368d52aadefe172f808a6aa6bd6
mryyan6 <zmryyan@gmail.com>
2016-09-12 20:19:48 -0500

    Create MonthViewDay.java

---
+++ src/MonthView/MonthViewDay.java
@@ -0,0 +1,43 @@
+package MonthView;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+
+
+public class MonthViewDay extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -917222811593509063L;
+	
+
+	private Integer day;
+	
+	public MonthViewDay(Integer dayNum, Dimension d){
+		day = dayNum;
+		setMinimumSize(d);
+		setMaximumSize(d);
+		setPreferredSize(d);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		JLabel label;
+		if(dayNum == null){
+			label = new JLabel("");
+		}else{
+			label = new JLabel(dayNum+"");
+		}
+		label.setForeground(Color.WHITE);
+		add(label);
+	}
+	
+	public int getDay(){
+		return day;
+	}
+
+
+}
