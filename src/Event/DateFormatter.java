26a682564f9b8f404c90909baab5fce74d569526
Michael Navickas <mnavickas14@gmail.com>
2016-09-13 13:00:52 -0500

    Add ability to convert Strings to dates

---
+++ src/Event/DateFormatter.java
@@ -0,0 +1,26 @@
+package Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatter {
+	
+	private static SimpleDateFormat dateFormatter = new SimpleDateFormat("MM-dd-yyyy");;
+	private static SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss");
+	public static SimpleDateFormat getFormat(){
+		return dateFormatter;
+	}
+	public static String format(FormatTypes type, Date date){
+		switch(type)
+		{
+		case Date:
+			return dateFormatter.format(date);
+		case DateTime:
+			return dateTimeFormatter.format(date);
+		default:
+			return dateFormatter.format(date);
+		
+		}
+		
+	}
+}
