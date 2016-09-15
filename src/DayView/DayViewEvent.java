536e969090bfe15c4e873faca125d0cb1d28301c
Michael Navickas <mnavickas14@gmail.com>
2016-09-14 21:00:05 -0500

    Add viewer for clicking on day view events

--- src/DayView/DayViewEvent.java
+++ src/DayView/DayViewEvent.java
@@ -1,12 +1,15 @@
 package DayView;
 
 import java.awt.Color;
+import java.text.ParseException;
+import java.util.Calendar;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
+import Event.DateFormatter;
 import Event.Event;
 
 public class DayViewEvent extends JPanel{
@@ -15,8 +18,12 @@ public class DayViewEvent extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = 2318788304336791454L;
-	
+	private int day;
+	private int month;
+	private int year;
+	private final Event thisEvent;
 	public DayViewEvent(Event e){
+		thisEvent = e;
 		//this.setBackground(Color.BLACK);
 		Border doubleBorder = BorderFactory.createCompoundBorder(
 				BorderFactory.createRaisedBevelBorder(), 
@@ -27,6 +34,16 @@ public class DayViewEvent extends JPanel{
 													doubleBorder, 
 													BorderFactory.createLineBorder(Color.BLACK)
 													));
+	
+		Calendar cal = Calendar.getInstance();
+		try {
+			cal.setTime(DateFormatter.getFormat().parse(e.Date));
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+		}
+		day = cal.get(Calendar.DAY_OF_MONTH);
+		month = cal.get(Calendar.MONTH);
+		year = cal.get(Calendar.YEAR);
 		
 		JLabel name = new JLabel(e.Name);
 		name.setForeground(Color.BLACK);
@@ -34,6 +51,27 @@ public class DayViewEvent extends JPanel{
 		
 		this.setToolTipText(e.Description);
 
+	}
+
+
+	public int getDay() {
+		// TODO Auto-generated method stub
+		return day;
+	}
+
+
+	public int getMonth() {
+		// TODO Auto-generated method stub
+		return month;
+	}
+
+	public int getYear() {
+		// TODO Auto-generated method stub
+		return year;
+	}
+	
+	public Event getEvent(){
+		return thisEvent;
 	}
 
 }
--- src/Listeners/EventPicker.java
+++ src/Listeners/EventPicker.java
@@ -3,36 +3,46 @@ package Listeners;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JOptionPane;
+
+import DayView.DayViewEvent;
+import DayView.DayViewEventViewer;
+import Exceptions.InstanceOverflowException;
+
 public class EventPicker implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		System.out.println("WooHoo");
+		if(arg0.getClickCount() == 2){
+			DayViewEvent e = (DayViewEvent)arg0.getSource();
+
+			try {
+				DayViewEventViewer.create(e);
+			} catch (InstanceOverflowException e1) {
+				JOptionPane.showMessageDialog(null, "Please finish what you are doing before creating another.");
+			}
+		}
 		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		//Intentionally blank
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		//Intentionally blank
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		//Intentionally blank
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		//Intentionally blank
 	}
 
 }
